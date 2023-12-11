cab934803614859b2f3714d0b491302fa9f695d2
demos1981 <50639080+demos1981@users.noreply.github.com>
2023-12-11 23:13:52 +0200

    installTsc
    instll tsc and ts-node

---
+++ src/typescript.js
@@ -0,0 +1,6 @@
+var text = "Hello Kitty";
+var isBoolean = true;
+var number = 54;
+var symbol;
+var array = [1, 2, 3];
+console.log(number);
---
+++ src/typescript.ts
@@ -0,0 +1,7 @@
+let text = "Hello Kitty";
+let isBoolean: boolean = true;
+let number: number = 54;
+let symbol: symbol;
+let array = [1, 2, 3];
+
+console.log(array);
