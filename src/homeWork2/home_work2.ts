f7d0ef93f43f76d2added302cdddd383fd2a617d
demos1981 <50639080+demos1981@users.noreply.github.com>
2023-12-18 00:44:59 +0200

    refactoringClasses

--- src/homeWork2/home_work2.ts
+++ src/homeWork2/home_work2.ts
@@ -1,41 +1,114 @@
 //Ви маєте JS код, який необхідно розширити анотацією примітивів, масивів, об'єктів (за необхідності),
 // подумати над використанням перерахувань,
 //а також реалізувати описані у вигляді коментарів властивості та методи.
-
+interface Lecturers {
+  name: string;
+  surname: string;
+  position: number;
+  company: string;
+  experience: string;
+  courses: string;
+  contacts: string;
+}
 class School {
   // implement 'add area', 'remove area', 'add lecturer', and 'remove lecturer' methods
 
-  _areas = [];
-  _lecturers = []; // Name, surname, position, company, experience, courses, contacts
+  private _areas: string[] = [];
+  private _lecturers: Lecturers[] = []; // Name, surname, position, company, experience, courses, contacts
 
-  get areas() {
-    this._areas;
+  get areas(): string[] {
+    return this._areas;
   }
 
-  get lecturers() {
-    this._lecturers;
+  get lecturers(): Lecturers[] {
+    return this._lecturers;
+  }
+
+  addArea(area: string): void {
+    this._areas.push(area);
+  }
+
+  removeArea(area: string): void {
+    const areaItem = this._areas.indexOf(area);
+    if (areaItem !== -1) {
+      this._areas.splice(areaItem, 1);
+    }
+  }
+
+  addLecturer(lecturer: Lecturers): void {
+    this._lecturers.push(lecturer);
+  }
+
+  removeLecturer(lecturer: Lecturers): void {
+    const lecturerItem = this._lecturers.indexOf(lecturer);
+    if (lecturerItem !== -1) {
+      this._lecturers.splice(lecturerItem, 1);
+    }
   }
 }
 
 class Area {
   // implement getters for fields and 'add/remove level' methods
-  _levels = [];
-  _name;
+  private _levels: string[] = [];
+  private _name: string;
 
-  constructor(name) {
+  constructor(name: string) {
     this._name = name;
+  }
+  get levels(): string[] {
+    return this._levels;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  addLevel(levels: string): void {
+    this._levels.push(levels);
+  }
+
+  removeLevel(levels: string): void {
+    const levelItem = this._levels.indexOf(levels);
+    if (levelItem !== -1) {
+      this._levels.splice(levelItem, 1);
+    }
   }
 }
 
 class Level {
   // implement getters for fields and 'add/remove group' methods
 
-  _groups;
-  _name;
+  private _groups: number[] = [];
+  private _name: string;
+  private _description: string;
 
-  constructor(name, description) {
-    this.name = name;
+  constructor(name: string, description: string, groups: number[]) {
+    this._name = name;
     this._description = description;
+    this._groups = groups;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get groups(): number[] {
+    return this._groups;
+  }
+
+  addGroup(group: number): void {
+    this._groups.push(group);
+  }
+
+  removeGroupe(groupe: number): void {
+    const groupeItem = this._groups.indexOf(groupe);
+    if (groupeItem !== -1) {
+      this._groups.splice(groupeItem, 1);
+    }
   }
 }
 
