1939dddb7c41672a9426c03359f35322cafd09bc
demos1981 <50639080+demos1981@users.noreply.github.com>
2024-01-25 20:57:02 +0200

    addHomeWork9

---
+++ src/homeWork9/home_work9.js
@@ -0,0 +1,16 @@
+function exampleFunction1() {
+    return 'Hello';
+}
+function exampleFunction2() {
+    return 42;
+}
+function exampleFunction3() {
+    return true;
+}
+var result1 = 'Some string';
+var result2 = 123;
+var result3 = false;
+function exampleFunction(value) {
+    return value.toString();
+}
+var funcTuple = ['new', 42];
