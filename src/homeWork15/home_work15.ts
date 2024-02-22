209936db32acd568207f81fceb9abd9f7dff5602
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-02-22 22:16:53 +0200

    addPATTERN

--- src/homeWork15/home_work15.ts
+++ src/homeWork15/home_work15.ts
@@ -1,3 +1,12 @@
+//Вам необхідно розширити поведінку прикладу з банківським рахунком. 
+//Додайте до нашої програми компонент Bank, який вміє створювати ти закривати акаунти для клієнтів. 
+//Кліент може мати декілька аккаунтів з різними типами валют. Bank повинен бути Singleton!
+
+//* Для тих, кому цікаво ускладнити - додайте можливість ставити транзацкції у чергу та мати можливість їх повторювати чи відміняти (Command)
+
+
+
+
 interface IObserver {
   update(observable: IObservable): void;
 }
@@ -7,14 +16,30 @@ interface IObservable {
   attach(observer: IObserver): void;
   detach(observer: IObserver): void;
   notify(): void;
+
 }
 
 
-interface BankClient {
+interface IBankClient {
   firstName: string;
   lastName: string;
 }
+enum CurrencyTypeEnum {
+EUR = 'EUR',
+USD = 'USD',
+GBP = 'GBP',
+UAH = 'UAH',
+}
+interface ICurrencyConversionStrategy {
+    convert(amount:number,currency:CurrencyTypeEnum):number;
 
+}
+
+interface IBank{
+
+      openBankAccount():void;
+      closeBankAccount():void;
+}
 
 abstract class Observable implements IObservable {
   private readonly observers: IObserver[] = [];
@@ -53,7 +78,20 @@ abstract class Observable implements IObservable {
     }
   }
 }
+class Bank implements IBank{
+    private static instance: Bank;
+    private constructor(){}
+    public static getInstance():Bank{
+        if(!Bank.instance){
+            Bank.instance = new Bank();
+        }
+        return Bank.instance;
+    }
+ public openBankAccount():void{}
+ public closeBankAccount():void{}
 
+ 
+}
 
 class BankAccount extends Observable {
   private readonly currency: string;
@@ -64,7 +102,7 @@ class BankAccount extends Observable {
 
 
   constructor(
-    client: BankClient,
+    client: IBankClient,
     currency: string,
     conversionStrategy: ICurrencyConversionStrategy
   ) {
@@ -86,7 +124,7 @@ class BankAccount extends Observable {
   }
 
 
-  public set holderName({ firstName, lastName }: BankClient) {
+  public set holderName({ firstName, lastName }: IBankClient) {
     if (!firstName.trim()) throw new Error(`Client first name can't be empty!`);
     if (!lastName.trim()) throw new Error(`Client last name can't be empty!`);
 
@@ -160,3 +198,82 @@ class PushNotification implements IObserver {
     );
   }
 }
+
+//інтерфейс або абстрактний клас, що визначає метод execute() для виконання дії.
+interface IBankCommand {
+  execute(): void;
+}
+//Конкретні команди: реалізації команди, які інкапсулюють певні дії (наприклад, TurnOnLightCommand, OpenFileCommand).
+class BankOperationRepeate implements IBankCommand {
+    
+  constructor(private receiver: Receiver) {
+  
+  }
+
+ public execute(): void {
+    this.receiver.repeateAction('repeate');
+    
+  }
+}
+
+class BankOperationClose implements IBankCommand {
+  constructor(private receiver: Receiver) {}
+
+   public execute(): void {
+    this.receiver. closeAction('close');
+  }
+}
+//Приймач: об’єкт, який фактично виконує дію (світло, файл). Він викликається методом execute() команди
+class Receiver {
+  repeateAction(repeate:string): void {
+    console.log(`Repeate operation (${repeate}.)`);
+  }
+
+  closeAction(close:string): void {
+    console.log(`Close operation (${close}.)`);
+  }
+}
+//Виклик: об’єкт, який запускає команди (Кнопка, Запис меню). Він не виконує їх безпосередньо, а делегує команді.
+class Invoker {
+  private commands: IBankCommand[] = [];
+
+  addCommand(command: IBankCommand): void {
+    this.commands.push(command);
+  }
+
+  executeCommands(): void {
+    for (const command of this.commands) {
+      command.execute();
+    }
+    this.commands = []; // Clear executed commands
+  }
+}
+
+// Usage example
+const receiver = new Receiver();
+const commandA = new BankOperationRepeate(receiver);
+const commandB = new BankOperationClose(receiver);
+
+const invoker = new Invoker();
+invoker.addCommand(commandA);
+invoker.addCommand(commandB);
+
+invoker.executeCommands();
+
+
+
+
+
+
+function clientCode() {
+    const bank1 = Bank.getInstance();
+    const bank2 = Bank.getInstance();
+
+    if (bank1 === bank2) {
+        console.log('Bank is empty.');
+    } else {
+        console.log('Bank failed, not shure operation is correctly.');
+    }
+}
+
+clientCode();
